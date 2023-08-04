6ad6d9b8ef4ed283b5e395598161a291d2aa0cda
Julia <yuliya_lysenko_03@mail.ru>
2023-08-05 00:07:11 +0300

    T1

--- T1/Program.cs
+++ T1/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+// 4 -> да
+// -3 -> нет
+// 7 -> нет
+
+Console.WriteLine("Пожалуйста введите число,");
+Console.WriteLine("чтобы узнать является ли оно чётным.");
+Console.Write("Введите число: ");
+int num = int.Parse(Console.ReadLine());
+int remain = num % 2;
+if (remain == 0)
+{
+Console.WriteLine("Число " + num + " чётное");
+}
+else
+{
+Console.WriteLine("Число " + num + " нечётное");
+}
