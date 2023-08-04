53a45a51f3a26a46806389b933edc28c4c084cd9
Julia <yuliya_lysenko_03@mail.ru>
2023-08-05 00:17:16 +0300

    T3
    Signed-off-by: Julia <yuliya_lysenko_03@mail.ru>

---
+++ T3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+﻿Console.WriteLine("Пожалуйста введите 3 числа,");
+Console.WriteLine("чтобы узнать какое большее, а какое меньшее.");
+Console.Write("Введите первое число: ");
+int num1 = int.Parse(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int num2 = int.Parse(Console.ReadLine());
+Console.Write("Введите третье число: ");
+int num3 = int.Parse(Console.ReadLine());
+int max = 0;
+if (num1 > max) max = num1;
+if (num2 > max) max = num2;
+if (num3 > max) max = num3;
+Console.Write("Максимальное значение имеет число " + max);
